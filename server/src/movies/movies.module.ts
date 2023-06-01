717f810691ee498449655259e4a00b7d9c862ac1
ThapLV <quangthap9x@gmail.com>
2023-06-01 16:41:08 +0700

    remove gateway

--- server/src/movies/movies.module.ts
+++ server/src/movies/movies.module.ts
@@ -4,12 +4,11 @@ import { Movie } from './models/movie.model';
 import { MoviesController } from './movies.controller';
 import { MoviesProvider } from './movies.provider';
 import { MoviesService } from './movies.service';
-import { MoviesGateway } from './movie.gateway';
 import { MoviesEmitter } from './emitters/movie.emitter';
 
 @Module({
   controllers: [MoviesController],
-  providers: [MoviesService, MoviesGateway, MoviesEmitter, ...MoviesProvider],
+  providers: [MoviesService, MoviesEmitter, ...MoviesProvider],
   imports: [SequelizeModule.forFeature([Movie])],
   exports: [SequelizeModule],
 })
